1f62bab0e5758badffb9f638e4a8418df4c509f3
Maikel Both <maikelboth@gmail.com>
2020-10-18 00:51:00 +0200

    Create passwords.php
    Added flemish translations for passwords.php

---
+++ resources/lang/nl_BE/passwords.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Copyright (c) 2020 Max Korlaar
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ *  Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions, a visible attribution to the original author(s)
+ *   of the software available to the public, and the following disclaimer
+ *   in the documentation and/or other materials provided with the distribution.
+ *
+ *  Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+    return [
+
+        /*
+        |--------------------------------------------------------------------------
+        | Password Reset Language Lines
+        |--------------------------------------------------------------------------
+        |
+        | The following language lines are the default lines which match reasons
+        | that are given by the password broker for a password update attempt
+        | has failed, such as for an invalid token or invalid new password.
+        |
+        */
+
+        'reset'     => 'Uw wachtwoord is gereset!',
+        'sent'      => 'We hebben uw link voor het opnieuw instellen van uw wachtwoord gemaild!',
+        'token'     => 'Dit token voor het opnieuw instellen van uw wachtwoord is ongeldig.',
+        'user'      => "We kunnen geen gebruiker met dat e-mailadres vinden.",
+        'throttled' => 'Wacht alstublieft voordat u het opnieuw probeert.',
+
+    ];
