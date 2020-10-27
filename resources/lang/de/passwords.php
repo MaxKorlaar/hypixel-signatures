c642a7f920e05dfec631e57bd24fb9c4dcd24525
Tim Schneider <tim.schneider1337@gmail.com>
2020-10-27 17:52:50 +0100

    Update passwords.php

--- resources/lang/de/passwords.php
+++ resources/lang/de/passwords.php
@@ -44,7 +44,7 @@
         */
 
         'reset'     => 'Dein Passwort wurde zurück gesetzt!',
-        'sent'      => 'Wir haben Ihren Link zum Zurücksetzen Ihres Passworts per E-Mail gesendet!',
+        'sent'      => 'Wir haben Ihnen den Link zum Zurücksetzen Ihres Passworts per E-Mail gesendet!',
         'token'     => 'Dieses Token zum Zurücksetzen des Passworts ist ungültig.',
         'user'      => "Wir können keinen Benutzer mit dieser E-Mail-Adresse finden.",
         'throttled' => 'Bitte warten Sie, bevor Sie es erneut versuchen.',
