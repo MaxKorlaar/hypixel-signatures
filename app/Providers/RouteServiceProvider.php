8da903729fe818932ea34bbf929b885262279f05
Max Korlaar <max@maxkorlaar.com>
2025-07-11 01:24:43 +0800

    Add throttling middleware to player and friends routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -73,19 +73,27 @@
          */
         protected function configureRateLimiting(): void {
             RateLimiter::for('player-status', static function (Request $request) {
-                return Limit::perHour(120)->by($request->ip());
+                return Limit::perHour(120)->by($request->ip())->response(function () {
+                    return response()->view('errors.429', ['retry_after' => 3600], 429);
+                });
             });
 
             RateLimiter::for('friends', static function (Request $request) {
-                return Limit::perHour(200)->by($request->ip());
+                return Limit::perHour(200)->by($request->ip())->response(function () {
+                    return response()->view('errors.429', ['retry_after' => 3600], 429);
+                });
             });
 
             RateLimiter::for('guild', static function (Request $request) {
-                return Limit::perHour(400)->by($request->ip());
+                return Limit::perHour(400)->by($request->ip())->response(function () {
+                    return response()->view('errors.429', ['retry_after' => 3600], 429);
+                });
             });
 
             RateLimiter::for('player', static function (Request $request) {
-                return Limit::perHour(600)->by($request->ip());
+                return Limit::perHour(600)->by($request->ip())->response(function () {
+                    return response()->view('errors.429', ['retry_after' => 3600], 429);
+                });
             });
         }
 
